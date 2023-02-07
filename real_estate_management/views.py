462e9fc92a9f3dbbedc9272411e654469df620e5
dlupianez <dlupianez@gmail.com>
2023-02-06 21:37:04 -0300

    modificaciones al about me

--- real_estate_management/views.py
+++ real_estate_management/views.py
@@ -10,7 +10,7 @@ def about_me(request):
         'greet':'Hi people!',
         'name':'David Lupiañez',
         'age':39,
-        'excerpt':'Bachelor degree in information systems, with extensive experience in various areas of software development. Strongly achievement-oriented and with outstanding teamwork skills. I have developed in numerous technologies and languages, currently I am specializing in Python and Django Framework.',
+        'excerpt':'I have a Bachelor degree in information systems, with extensive experience in various areas of software development. I am strongly achievement-oriented with outstanding teamwork skills. I have developed in numerous technologies and languages, currently I am specializing in Python and Django Framework.',
         'project':'Project is about apartment rentals, so applications need to be able to handle apartments, tenants and rental objects, and their relationships.',
         'country':'Argentina',
         'location':'Mendoza',
